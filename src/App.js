10e914d327604d021e24f4ec5d8a59c86b8464f9
Nan Yuan <nan.yuan.dev@gmail.com>
2020-01-07 22:28:01 +1100

    Set style files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(()=>{
+    //Init Materialize JS
+    M.AutoInit();
+  })
   return (
     <div className="App">
       <div>Team Logger</div>
