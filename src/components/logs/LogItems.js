f9b19e55cb617ab31793f2a850874f994bd85ecc
Nan Yuan <nan.yuan.dev@gmail.com>
2020-01-17 23:28:57 +1100

    Change highlight color

--- src/components/logs/LogItems.js
+++ src/components/logs/LogItems.js
@@ -8,7 +8,7 @@ const LogItems = ({ log }) => {
       <div>
         <a
           href="#edit-log-modal"
-          className={`modal-trigger ${log.attention ? "red-text" : "blue-text"}`}
+          className={`modal-trigger ${log.attention ? "orange-text" : "blue-text"}`}
         >
           {log.message}
         </a>
