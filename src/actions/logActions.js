d743e40354a64249d65cbe71d833dab9e79eb911
Nan Yuan <nan.yuan.dev@gmail.com>
2020-01-19 23:42:17 +1100

    Add new log action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR,ADD_LOG } from "./types";
 
 // export const getLogs = () => {
 //   return async (dispatch) => {
@@ -37,4 +37,25 @@ export const setLoading = () => {
   return {
     type: SET_LOADING
   };
+};
+
+// Add new log
+export const addLogs = (log) => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch("/logs");
+    const res = await fetch('./')
+
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
 };
