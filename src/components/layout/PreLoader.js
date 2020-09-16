94c85b902f110f17efd52f04f1bcd57ddd27e0b2
Nan Yuan <nan.yuan.dev@gmail.com>
2020-09-16 12:01:28 +1000

    Add logItem and PreLoader component

---
+++ src/components/layout/PreLoader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PreLoader = () => {
+  return (
+    <div>
+      <div className="progress blue lighten-4">
+        <div className="indeterminate blue"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
